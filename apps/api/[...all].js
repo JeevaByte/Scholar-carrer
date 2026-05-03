48454fd3f5695ed581c0d64ab37ba1f330c3b791
Jeevanantham Balakrishnan <jeevananthambalakrishnan@Jeevananthams-MacBook-Air.local>
2026-05-03 10:16:44 +0100

    fix(vercel): remove conflicting handler js files

--- apps/api/[...all].js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_js_1 = require("../dist/app.js");
-// Vercel serverless entrypoint: export the Express request handler directly.
-exports.default = app_js_1.app;
